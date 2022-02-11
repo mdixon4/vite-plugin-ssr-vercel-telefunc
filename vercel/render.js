d469356ea94b35fa014760dafad62b1d019cb981
Matt Dixon <mdixon4@gmail.com>
2022-02-11 22:58:53 +1100

    Attempt to make telefunc work in Vercel handler

--- vercel/render.js
+++ vercel/render.js
@@ -1,6 +1,8 @@
 import { createPageRenderer } from 'vite-plugin-ssr'
+const { telefunc, telefuncConfig, provideTelefuncContext } = require('telefunc')
 // `importBuild.js` enables Vercel to bundle our serverless functions, see https://vite-plugin-ssr.com/vercel and https://vite-plugin-ssr.com/importBuild.js
 import '../dist/server/importBuild.js'
+import '../dist/server/importTelefuncFiles.js'
 
 const renderPage = createPageRenderer({ isProduction: true })
 
@@ -8,6 +10,20 @@ export default async function handler(req, res) {
   const { url } = req
 
   console.log('Request to url:', url)
+
+  if (url.startsWith('/_telefunc')) {
+    // provideTelefuncContext({})
+    const body = await readTextStream(req)
+    const httpResponse = await telefunc({ url, method: req.method, body })
+    if (httpResponse) {
+      const { body, statusCode, contentType } = httpResponse
+
+      res.statusCode = statusCode
+      res.setHeader('content-type', contentType)
+      res.end(body)
+      return
+    }
+  }
 
   const pageContextInit = { url }
   const pageContext = await renderPage(pageContextInit)
@@ -24,3 +40,12 @@ export default async function handler(req, res) {
     res.end(body)
   }
 }
+
+
+const readTextStream = readableStream =>
+  new Promise(resolve => {
+    let text = ''
+    readableStream.setEncoding('utf8')
+    readableStream.on('data', chunk => (text += chunk))
+    readableStream.on('end', () => resolve(text))
+  })
